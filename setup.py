3fe91e66a8578d09f4605a8cd4cfc25f0ca83d7e
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2020-01-14 23:31:18 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyk4a",
+    version="0.0.1",
+    author="The HexOps Authors",
+    author_email="stephen.gutekanst@gmail.com",
+    description="Python 3 bindings for the Azure Kinect SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hexops/Azure-Kinect-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
