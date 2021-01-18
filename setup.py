57e487f70dd33000ae9620b6276e9cc319f70182
Stephen Gutekanst <stephen@hexops.com>
2021-01-18 12:15:48 -0700

    add LICENSE (MIT / Apache dual license)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="k4a",
-    version="0.0.1",
+    version="1.1.0",
     author="The HexOps Authors",
     author_email="stephen.gutekanst@gmail.com",
     description="Python 3 bindings for the Azure Kinect SDK",
@@ -16,6 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
